53c4141e598dab20aa068a15d205d58b64ca813a
Alexey.Piksin <hekpojiog@list.ru>
2022-01-26 17:15:31 +0300

    add input

--- test-1.py
+++ test-1.py
@@ -1,9 +1,18 @@
 # работает для всех 1e-n, где 1 < n < 324, а grade = n + 1
-number = 1e-155
-grade = 156
+number = float(input('Введите число в экспоненциальном виде: '))
+num_str = str(number)
+grade_str = ''
+point = False
+for char in num_str:
+    if point:
+        grade_str += char
+    if char == 'e':
+        point = True
+grade = abs(int(grade_str)) + 1
+print(grade)
 i = 0
 while abs(number) < 1:
     i += 1
     number = round(number, grade - i)
     number *= 10
-    print(number)
+    print(int(number))
